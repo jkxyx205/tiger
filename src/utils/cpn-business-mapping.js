e986009a6b694c2d784e871c8f74cb1e10420d0b
rick <1050216579@qq.com>
2019-09-17 17:40:09 +0800

    Format code

--- src/utils/cpn-business-mapping.js
+++ src/utils/cpn-business-mapping.js
@@ -1,17 +1,17 @@
 const SYSTEM = 0
 const CPN_SITE = 1
 
-let mappings = {
+const mappings = {
   [SYSTEM]: [
     {
       id: 0,
       name: '系统信息'
-     },
+    }
   ],
   [CPN_SITE]: [
     {
-     id: 10,
-     name: '网站留言'
+      id: 10,
+      name: '网站留言'
     },
     {
       id: 11,
